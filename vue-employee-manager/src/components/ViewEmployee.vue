01d6a2eb40623b311d8440e34836250518830bfe
ArkejGit <robert.krawczyk92@gmail.com>
2018-01-02 11:47:09 +0100

    Fetch data with information about specific employee

--- vue-employee-manager/src/components/ViewEmployee.vue
+++ vue-employee-manager/src/components/ViewEmployee.vue
@@ -5,11 +5,43 @@
 </template>
 
 <script>
+import db from './firebaseInit'
+
 export default {
   name: 'view-employee',
   data () {
     return {
-
+      employee_id: null,
+      name: null,
+      dept: null,
+      position: null
+    }
+  },
+  beforeRouterEnter (to, from, next) {
+    db.collection('employees').where('employee_id', '==', to.params.employee_id).get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        next(vm => {
+          vm.employee_id = doc.data().employee_id
+          vm.name = doc.data().name
+          vm.dept = doc.data().dept
+          vm.position = doc.data().position
+        })
+      })
+    })
+  },
+  watch: {
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData () {
+      db.collection('employees').where('employee_id', '==', this.$route.params.employee_id).get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          this.employee_id = doc.data().employee_id
+          this.name = doc.data().name
+          this.dept = doc.data().dept
+          this.position = doc.data().position
+        })
+      })
     }
   }
 }
