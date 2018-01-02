31f2dd966bf68b2b463521d9f36d3b13f44b3530
ArkejGit <robert.krawczyk92@gmail.com>
2018-01-02 11:26:19 +0100

    Add icon with link to ViewEmployee for every employee in list

--- vue-employee-manager/src/components/Dashboard.vue
+++ vue-employee-manager/src/components/Dashboard.vue
@@ -7,6 +7,10 @@
       <li v-for="employee in employees" v-bind:key="employee.id" class="collection-item">
       	<div class="chip">{{ employee.dept }}</div>
       	{{ employee.employee_id }} : {{ employee.name }}
+
+      	<router-link class="secondary-content" v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
+      	  <i class="fa fa-eye"></i>
+      	</router-link>
       </li>
     </ul>
 
