45d7741fec2b6529d5bf2fc548be7cacf82c2cd3
ArkejGit <robert.krawczyk92@gmail.com>
2018-01-02 11:05:07 +0100

    Modify files according to eslint rules

--- vue-employee-manager/src/components/firebaseConfig.js
+++ vue-employee-manager/src/components/firebaseConfig.js
@@ -1,8 +1,8 @@
 export default {
-    apiKey: "AIzaSyBsHjlX0yTm_3Fy073Dq0YDC3dw8MD8VXs",
-    authDomain: "vue-employee-manager-fd1e5.firebaseapp.com",
-    databaseURL: "https://vue-employee-manager-fd1e5.firebaseio.com",
-    projectId: "vue-employee-manager-fd1e5",
-    storageBucket: "vue-employee-manager-fd1e5.appspot.com",
-    messagingSenderId: "144967142639"
+  apiKey: 'AIzaSyBsHjlX0yTm_3Fy073Dq0YDC3dw8MD8VXs',
+  authDomain: 'vue-employee-manager-fd1e5.firebaseapp.com',
+  databaseURL: 'https://vue-employee-manager-fd1e5.firebaseio.com',
+  projectId: 'vue-employee-manager-fd1e5',
+  storageBucket: 'vue-employee-manager-fd1e5.appspot.com',
+  messagingSenderId: '144967142639'
 }
