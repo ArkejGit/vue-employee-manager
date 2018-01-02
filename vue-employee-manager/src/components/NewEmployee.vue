cfea7e9701df07698636e8faa097e2a2cef80a17
ArkejGit <robert.krawczyk92@gmail.com>
2018-01-02 15:08:45 +0100

    Create function for adding new employee to database

--- vue-employee-manager/src/components/NewEmployee.vue
+++ vue-employee-manager/src/components/NewEmployee.vue
@@ -35,11 +35,28 @@
 </template>
 
 <script>
+import db from './firebaseInit'
+
 export default {
   name: 'new-employee',
   data () {
     return {
-
+      employee_id: null,
+      name: null,
+      dept: null,
+      position: null
+    }
+  },
+  methods: {
+    saveEmployee () {
+      db.collection('employees').add({
+        employee_id: this.employee_id,
+        name: this.name,
+        dept: this.dept,
+        position: this.position
+      })
+      .then(docRef => this.$router.push('/'))
+      .catch(error => console.log(error))
     }
   }
 }
