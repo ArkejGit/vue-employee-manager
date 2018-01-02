e8c6101f384105c2e5f58351408eb31c9c766777
ArkejGit <robert.krawczyk92@gmail.com>
2018-01-02 11:08:48 +0100

    Fetch employees data from firestore

--- vue-employee-manager/src/components/Dashboard.vue
+++ vue-employee-manager/src/components/Dashboard.vue
@@ -11,12 +11,28 @@
 </template>
 
 <script>
+import db from './firebaseInit'
+
 export default {
   name: 'dashboard',
   data () {
     return {
-
+      employees: []
     }
+  },
+  created () {
+    db.collection('employees').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        const data = {
+          'id': doc.id,
+          'employee_id': doc.data().employee_id,
+          'name': doc.data().name,
+          'dept': doc.data().dept,
+          'position': doc.data().position
+        }
+        this.employees.push(data)
+      })
+    })
   }
 }
 </script>
