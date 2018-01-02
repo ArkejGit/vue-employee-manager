4e336f8c66c436cf345b331b16efd5d1f3fab09d
ArkejGit <robert.krawczyk92@gmail.com>
2018-01-02 10:23:44 +0100

    Create html structure for navbar

--- vue-employee-manager/src/components/Navbar.vue
+++ vue-employee-manager/src/components/Navbar.vue
@@ -1,5 +1,11 @@
 <template>
-	<div id="navbar">
-		Navbar
-	</div>
+  <nav>
+    <div class="nav-wrapper blue">
+  	  <div class="container">
+  	    <router-link to="/" class="brand-logo">
+  	      Employee Manager
+  	    </router-link>
+  	  </div>
+  	</div>
+  </nav>
 </template>
