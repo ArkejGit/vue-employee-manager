f47e42d5c398ed5e4c2ed461fc481f5082fb02c8
ArkejGit <robert.krawczyk92@gmail.com>
2018-01-02 10:35:01 +0100

    Add floating button for creating new employee

--- vue-employee-manager/src/components/Dashboard.vue
+++ vue-employee-manager/src/components/Dashboard.vue
@@ -1,7 +1,13 @@
 <template>
-	<div id="dashboard">
-		<h3>Dashboard</h3>
-	</div>
+  <div id="dashboard">
+  	<h3>Dashboard</h3>
+
+  	<div class="fixed-action-btn">
+  	  <router-link to="/new" class="btn-floating btn-large deep-purple">
+  	  	<i class="fa fa-plus"></i>
+  	  </router-link>
+  	</div>
+  </div>
 </template>
 
 <script>
